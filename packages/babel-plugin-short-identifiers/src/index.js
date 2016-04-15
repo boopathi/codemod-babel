2a8ac7e2b4c3408d46825c58ac66aa8d51dc745b
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2016-04-15 17:19:54 +0200

    generator and handled cases

--- packages/babel-plugin-short-identifiers/src/index.js
+++ packages/babel-plugin-short-identifiers/src/index.js
@@ -1,28 +1,73 @@
 export default function ({types: t}) {
 
-  let state = [];
 
-  const identifierVistor = {
-    
-    Identifier(path) {
-      let newIdentifier = path.node.name.charAt(0);
-      if (!checkIfkeyExists(state, newIdentifier)) {
-        state.push(newIdentifier);
-        path.node.name = newIdentifier;
+  const isNotChangable = (path) => {
+    // Globals
+    if (path.scope.hasGlobal(path.node.name)) {
+      return true;
+    }
+    // import {component}
+    if (t.isImportSpecifier(path.parentPath.node)) {
+      // import {Component as myComponent}
+      if (path.parentKey === 'local') {
+        return false;
       }
+      return true;
+    }
+
+    // exports {component}
+    if (t.isExportSpecifier(path.parentPath.node)) {
+      // export {Component as myComponent}
+      if (path.parentKey === 'local') {
+        return false;
+      }
+      return true;
+    }
+
+    // object descructor
+
+    // object expression
+
+    //class methods and object methods
+
+      return false;
+  }
+
+  const identifierReplaceVisitor = {
+    Identifier(path) {
+      if (isNotChangable(path)) {
+        return;
+      }
+      path.node.name = this[path.node.name];
+    }
+  }
+
+  const identifierGrabVistor = {
+    Identifier(path) {
+      if (isNotChangable(path)) {
+        return;
+      }
+      this.add(path.node.name);
     }
   };
 
-  const checkIfkeyExists = (state, key) => {
-    state.map((x) => {
-        if (x === key) {
-          return true;
-        }
-    });
-    return false;
-  };
+  function* nameGen() {
+    yield* 'abcdefghijklmnopqrstuvwxyz'.split('');
+  }
 
   return {
-    visitor: identifierVistor
+    visitor: {
+      Program(path) {
+        let state = new Set();
+        let newState = {};
+        let it = nameGen();
+
+        path.traverse(identifierGrabVistor, state);
+        for(let item of state) {
+          newState[item] = it.next().value;
+        }
+        path.traverse(identifierReplaceVisitor, newState);
+      }
+    }
   };
 }
